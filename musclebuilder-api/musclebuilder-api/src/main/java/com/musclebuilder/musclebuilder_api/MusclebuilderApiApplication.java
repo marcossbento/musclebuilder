2a9ac980a71ccfaee8c7079f7aba44c70912aea7
marcossbento <marcossbento14@gmail.com>
2025-04-05 12:26:52 -0300

    Initial Commit

---
+++ musclebuilder-api/musclebuilder-api/src/main/java/com/musclebuilder/musclebuilder_api/MusclebuilderApiApplication.java
@@ -0,0 +1,13 @@
+package com.musclebuilder.musclebuilder_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusclebuilderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusclebuilderApiApplication.class, args);
+	}
+
+}
---
+++ musclebuilder-api/musclebuilder-api/src/test/java/com/musclebuilder/musclebuilder_api/MusclebuilderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musclebuilder.musclebuilder_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusclebuilderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
