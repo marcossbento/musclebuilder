bbf4cd3a4bf133eac6e04399a5cabd1f6a35db77
marcossbento <marcossbento14@gmail.com>
2025-04-28 08:16:20 -0300

    fix: move SecurityConfig from security to config package

--- musclebuilder-api/src/main/java/com/musclebuilder/config/SecurityConfig.java
+++ musclebuilder-api/src/main/java/com/musclebuilder/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.musclebuilder.security;
+package com.musclebuilder.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
