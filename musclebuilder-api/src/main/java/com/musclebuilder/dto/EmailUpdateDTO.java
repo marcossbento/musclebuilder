f778cb9d9e60fbae4fa7865d98bbe7214c50a722
marcossbento <marcossbento14@gmail.com>
2025-06-16 16:30:06 -0300

    fix: remove lombok and use record at Pass and Email update DTOs

--- musclebuilder-api/src/main/java/com/musclebuilder/dto/EmailUpdateDTO.java
+++ musclebuilder-api/src/main/java/com/musclebuilder/dto/EmailUpdateDTO.java
@@ -2,18 +2,14 @@ package com.musclebuilder.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-public class EmailUpdateDTO {
+public record EmailUpdateDTO (
 
     @NotBlank(message = "O e-mail não pode estar em branco")
     @Email(message = "E-mail deve ter formato válido")
-    private String newEmail;
+    String newEmail,
 
     @NotBlank(message = "A senha é obrigatória")
-    private String currentPassword;
+    String currentPassword
 
-}
+) {}
--- musclebuilder-api/src/main/java/com/musclebuilder/dto/PasswordUpdateDTO.java
+++ musclebuilder-api/src/main/java/com/musclebuilder/dto/PasswordUpdateDTO.java
@@ -2,21 +2,17 @@ package com.musclebuilder.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-public class PasswordUpdateDTO {
+public record PasswordUpdateDTO (
 
     @NotBlank(message = "A senha atual é obrigatória")
-    private String currentPassword;
+    String currentPassword,
 
     @NotBlank(message = "Nova senha é obrigatória")
     @Size(min = 8, message = "A senha deve conter no mínimo 8 caracteres")
-    private String newPassword;
+    String newPassword,
 
     @NotBlank(message = "A confirmação de senha é obrigatória")
-    private String confirmPassword;
+    String confirmPassword
 
-}
+) {}
