55dffe87002bed51f6a837f5caa769bf35a30dfd
marcossbento <marcossbento14@gmail.com>
2026-02-02 14:27:08 -0300

    git commit -m "chore(test): configure Testcontainers for integration testing"

---
+++ src/test/java/com/musclebuilder/AbstractIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.musclebuilder;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:15-alpine")
+            .withDatabaseName("musclebuilder-test-db")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    
+    @DynamicPropertySource
+    static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+}
