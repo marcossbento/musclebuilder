f61f65ea4ceacae74e281e45049665457775d8ea
marcossbento <marcossbento14@gmail.com>
2026-02-04 10:49:04 -0300

    build: configure Testcontainers and resolve H2 datasource conflicts
    - Force Docker API version to 1.45 in testcontainers.properties
    - Remove static H2 datasource config from application-test.properties to allow Testcontainers injection
    - Update AbstractIntegrationTest to manage dynamic datasource properties correctly

--- src/test/java/com/musclebuilder/AbstractIntegrationTest.java
+++ src/test/java/com/musclebuilder/AbstractIntegrationTest.java
@@ -8,7 +8,6 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class AbstractIntegrationTest {
 
     @Container
